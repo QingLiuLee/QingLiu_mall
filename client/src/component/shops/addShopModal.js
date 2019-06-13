e75a51d50069a2844d62615c986ad2a19997df55
liuhuihui <244701374@qq.com>
2019-06-13 17:17:28 +0800

    商铺创建及添加

--- client/src/component/shops/addShopModal.js
+++ client/src/component/shops/addShopModal.js
@@ -2,13 +2,10 @@ import React,{Component} from 'react';
 import {
     Form,
     Input,
-    Select,
-    AutoComplete,
+    Select
 } from 'antd';
 
 const { Option } = Select;
-const AutoCompleteOption = AutoComplete.Option;
-
 const residences = [
     {
         value: 'zhejiang',
@@ -53,13 +50,24 @@ class AddShopModal extends Component{
     constructor(props){
         super(props);
         this.state = {
-            confirmDirty: false,
-            autoCompleteResult: [],
+            img_list: ['img1','img2'],
+            sale_type: ["美妆","家电"],
         }
     }
 
+    // 售货类型
+    changeSaleType = () =>{
+
+    }
+
+    // 商铺图片
+    changeImgList = () =>{
+
+    }
+
     render (){
-        const {} = this.state;
+        const { img_list, sale_type } = this.state
+        const { shopDatas } = this.props;
         const { getFieldDecorator } = this.props.form;
 
         const formItemLayout = {
@@ -76,29 +84,92 @@ class AddShopModal extends Component{
             <div className="ql-modal">
                 <div className="ql-search">
                     <Form {...formItemLayout}>
-                        <Form.Item label="E-mail">
-                            {getFieldDecorator('email', {
+                        <Form.Item label="商铺名称" hasFeedback>
+                            {getFieldDecorator('org_name', {
+                                initialValue: shopDatas ? shopDatas.org_name : undefined,
                                 rules: [
                                     {
-                                        type: 'email',
-                                        message: 'The input is not valid E-mail!',
-                                    },
-                                    {
                                         required: true,
-                                        message: 'Please input your E-mail!',
+                                        message: '请输入商铺名称',
                                     },
-                                ],
-                            })(<Input />)}
+                                ]
+                            })(
+                                <Input />
+                            )}
                         </Form.Item>
-                        <Form.Item label="Password" hasFeedback>
-                            {getFieldDecorator('password', {
+                        <Form.Item label="商铺简介" hasFeedback>
+                            {getFieldDecorator('explain', {
+                                initialValue: shopDatas ? shopDatas.explain : undefined,
                                 rules: [
                                     {
                                         required: true,
-                                        message: 'Please input your password!',
+                                        message: '请输入商铺简介',
+                                    },
+                                ]
+                            })(
+                                <Input />
+                            )}
+                        </Form.Item>
+                        <Form.Item label="商铺图片" hasFeedback>
+                            {getFieldDecorator('img_list', {
+                                initialValue: shopDatas ? shopDatas.img_list : undefined,
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: '请输入商铺图片名称',
+                                    },
+                                ]
+                            })(
+                                <Select
+                                    mode="multiple"
+                                    style={{ width: '100%' }}
+                                    placeholder="Please select"
+                                    onChange={this.changeSaleType}
+                                >
+                                    {
+                                        img_list.length > 0 && img_list.map(item => {
+                                            return <Option key={item}>{item}</Option>
+                                        })
                                     }
-                                ],
-                            })(<Input.Password />)}
+                                </Select>
+                            )}
+                        </Form.Item>
+                        <Form.Item label="售货类型" hasFeedback>
+                            {getFieldDecorator('sale_type', {
+                                initialValue: shopDatas ? shopDatas.sale_type : undefined,
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: '请输入售货类型',
+                                    },
+                                ]
+                            })(
+                                <Select
+                                    mode="multiple"
+                                    style={{ width: '100%' }}
+                                    placeholder="Please select"
+                                    onChange={this.changeSaleType}
+                                >
+                                    {
+                                        sale_type.length > 0 && sale_type.map(item => {
+                                            return <Option key={item}>{item}</Option>
+                                        })
+                                    }
+                                </Select>
+                            )}
+                        </Form.Item>
+                        <Form.Item label="商铺管理员编码" hasFeedback>
+                            {getFieldDecorator('staff_code', {
+                                initialValue: shopDatas ? shopDatas.staff_code : undefined,
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: '请输入商铺管理员编码',
+                                    },
+                                ]
+                            })(
+                                <Input />
+                            )}
                         </Form.Item>
                     </Form>
                 </div>
--- client/src/component/shops/shops.js
+++ client/src/component/shops/shops.js
@@ -1,38 +1,46 @@
 import React,{Component} from 'react';
-import { Button } from 'antd';
+import { Button,message } from 'antd';
 import ComTable from '../common/ComTable';
 import ComModal from '../common/ComModal';
 import SearchForm from '../search';
 import AddShopModal from './addShopModal';
+import { post } from '../../utils/axiosUtil'
+import { getLocalStorage } from '../../utils/localStorage'
 
 /**
  * @author hui
  * @date 2019/4/28
  * @Description: 商铺管理
 */
-const storge = window.localStorage;
-let staff_code = '';
-if(storge.getItem('staff_code')){
-    staff_code = storge.getItem('staff_code')
-}
 export default class Profile extends Component{
     constructor(props){
         super(props);
         this.state = {
             shopUrl: '/v1/merchant/organization/create/info',
             addShopUrl: '/v1/merchant/organization/create/info',
+            editShopUrl: '/v1/merchant/organization/update/info',
             refresh: 0,//table改变时对应刷新变化值
-            postParam: {
-                org_name: "商家名2",
-                explain: "简介",
-                img_list: ["logo3","logo2"],
-                sale_type: ["美妆","家电"],
-                owner_code: staff_code
-            },
+            postParam: {},
             getParam: {},
             dataSource: [],
             visible: false,
-            imgs: 'logo2'
+            isAdd: true,
+            imgs: 'logo2',
+            shopDatas: {
+                org_name: undefined,
+                explain: undefined,
+                img_list: undefined,
+                sale_type: undefined,
+                staff_code: getLocalStorage('staff_code')
+            },
+            shopDatas2: {
+                explain: "商铺1-简介1",
+                img_list: "img1",
+                org_code: "M201906130428176",
+                org_name: "商铺11",
+                sale_type: "美妆",
+                staff_code: "S2019061204090878"
+            }
         }
     }
 
@@ -55,11 +63,59 @@ export default class Profile extends Component{
     // 创建商铺
     addModal = () =>{
         this.setState({
-            visible:true
+            visible:true,
+            isAdd: true
         })
     }
 
+    // 更新商铺
+    editModal = () =>{
+        this.setState({
+            visible:true,
+            isAdd: false
+        })
+    }
+
+    // 创建 | 保存
+    onSubmit = () =>{
+        this.refs.shopform.validateFields((err, formData) => {
+            if (!err) {
+                console.info('success');
+                const { isAdd, addShopUrl, editShopUrl, shopDatas2} = this.state
+                const url = isAdd ? addShopUrl : editShopUrl
+
+                if(!isAdd){
+                    formData.org_code = shopDatas2.org_code
+                    console.log(formData)
+                }
+                post(url,formData,null).then(res =>{
+                    if(isAdd){
+                        formData.org_code = res.data
+                    }else{
+                        message.success(res.data)
+                    }
+                    console.log(formData);
+                    this.setState({
+                        visible: false,
+                        shopDatas: formData
+                    })
+                }).catch(err =>{
+                    message.warning(err.data)
+                    this.setState({
+                        visible: false
+                    })
+                })
+            }
+        });
+    }
+
     render (){
+        const {
+            shopUrl, visible, isAdd, imgs,
+            shopDatas, shopDatas2,
+            refresh, postParam, getParam
+        } = this.state
+
         const columns = [
             {
                 title: '商铺名称',
@@ -93,53 +149,34 @@ export default class Profile extends Component{
             }
         ];
 
-        const { shopUrl, refresh, postParam, getParam, visible,imgs } = this.state
-
-        const data = [{
-            key: '1',
-            name: 'John Brown',
-            age: 32,
-            address: 'New York No. 1 Lake Park',
-        }, {
-            key: '2',
-            name: 'Jim Green',
-            age: 42,
-            address: 'London No. 1 Lake Park',
-        }, {
-            key: '3',
-            name: 'Joe Black',
-            age: 32,
-            address: 'Sidney No. 1 Lake Park',
-        }];
-
         return (
-            <div className="ql-home">
-                {/*创建商铺*/}
+            <div className="ql-main home">
+                {/* 创建|修改 商铺*/}
                 <ComModal
                     visible={visible}
                     onCancel={()=>this.setState({visible:false})}
-                    onSubmit={this.addModal}
-                    title='创建商铺'
+                    onSubmit={this.onSubmit}
+                    title={isAdd ? '创建商铺':'修改商铺'}
                 >
-                    <AddShopModal />
+                    <AddShopModal ref="shopform" shopDatas={shopDatas2}/>
                 </ComModal>
 
-                <div className="ql-home-search">
+                <div className="ql-main-search home-search">
                     <SearchForm onSearch={this.onSearch}/>
                 </div>
 
-                <div className="ql-home-btn">
+                <div className="ql-main-btns home-btn">
                     <Button onClick={this.addModal}>创建商铺</Button>
+                    <Button onClick={this.editModal}>更新商铺</Button>
                 </div>
 
-                <div className="ql-home-table">
+                <div className="ql-main-table home-table">
                     <ComTable
                         columns={columns}
                         url={shopUrl}
                         refresh={refresh}
                         postParam={postParam}
                         getParam={getParam}
-                        newDatas={data}
                     />
                 </div>
             </div>
